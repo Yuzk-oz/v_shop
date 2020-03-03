b7f929db07227d384935cd014aaf4acb6dde43b3
yuzk <1609270269@qq.com>
2020-03-03 19:38:39 +0800

    完成了login

---
+++ src/components/Login.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules='loginFormRules' label-width="0px" class="login_form">
+        <!-- 登录名 -->
+        <el-form-item prop='username'>
+          <el-input v-model='loginForm.username' prefix-icon='el-icon-user'></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop='password'>
+          <el-input v-model="loginForm.password" prefix-icon='el-icon-lock' type='password'></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type='primary' @click="login">登录</el-button>
+          <el-button type='info' @click="resetloginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      loginForm:{
+        username:'admin',
+        password:'123456'
+      },
+      loginFormRules:{
+        // 验证登录名和密码
+        username:[
+          {
+            required:true,
+            message:'请输入用户名',
+            trigger:'blur'
+          },{
+            min:5,
+            max:11,
+            message:'5-11个字符',
+            trigger:'blur'
+          }
+        ],
+          password:[ {
+            required:true,
+            message:'请输入密码',
+            trigger:'blur'
+          },{
+            min:3,
+            max:11,
+            message:'3-11个字符',
+            trigger:'blur'
+          }]
+      }
+    }
+  },
+  methods:{
+    resetloginForm(){
+      this.$refs.loginFormRef.resetFields()
+    },
+    login(){
+      this.$refs.loginFormRef.validate(async valid=>{
+        if(!valid) return
+        const { data: res } =await this.$http.post('login',this.loginForm)
+        if(res.meta.status!==200)return this.$message.error('登录失败!')
+       this.$message.success('登录成功!')
+
+
+      //  将成功之后的token保存到客户端的sessionStorage中
+      //  项目中除了登录之外的其他API接口,必须再登录之后才能访问
+      //  token只应再当前网站打开期间生效,所以将tokenbao保存再sessionStorage中
+      console.log(res);
+      window.sessionStorage.setItem('token',res.data.token)
+      //  通过编程式导航跳转到后台主页,路由地址式/home
+      this.$router.push('/home')
+
+
+
+
+
+      })
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  width: 100%;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .login_form{
+    position: absolute;
+    bottom:0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns{
+   display:flex;
+   justify-content: flex-end;
+
+  }
+}
+</style>>
+
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import './plugins/element.js'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+    // 配置请求根路径
+axios.defaults.baseURL = 'http://119.23.53.78:8888/api/private/v1/'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount('#app');
